85e3e03735658fcc609f1174085087d564a8c8e7
Bernard Austin <bernardcpa@gmail.com>
2023-04-11 17:52:05 -0500

    add path /

--- src/appcontainer.jsx
+++ src/appcontainer.jsx
@@ -76,7 +76,7 @@ const AppContainer =  (props) => {
                  <StickySidebar />                                           
             <Switch>    
                                              
-              <Route path="/index" component={Dashboard} />
+              <Route path="/" component={Dashboard} />
               <Route path="/indextwo" component={Indextwo} />
               <Route path="/indexthree" component={Indexthree} />
               <Route path="/indexfour" component={Indexfour} />
